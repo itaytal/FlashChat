56ea7c546e18205a0c11dc34274494a7efdfad8d
Globes Design <globes@Globess-iMac.local>
2019-01-08 08:36:49 +0200

    add chat

---
+++ Flash Chat/RegisterViewController.swift
@@ -0,0 +1,41 @@
+//
+//  RegisterViewController.swift
+//  Flash Chat
+//
+//  This is the View Controller which registers new users with Firebase
+//
+
+import UIKit
+import Firebase
+
+
+class RegisterViewController: UIViewController {
+
+    //Pre-linked IBOutlets
+
+    @IBOutlet var emailTextfield: UITextField!
+    @IBOutlet var passwordTextfield: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+ 
+  
+    @IBAction func registerPressed(_ sender: AnyObject) {
+        
+        //TODO: Set up a new user on our Firbase database
+        Auth.auth().createUser(withEmail: emailTextfield.text!, password: passwordTextfield.text!) {
+            (authResult, error) in
+            if error != nil {
+                print(error!)
+               
+            }
+            else {
+                print("itay: success")
+                 self.performSegue(withIdentifier: "goToChat", sender: self)
+            }
+        }
+    }
+}
